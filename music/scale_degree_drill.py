1bfe8ed53135420b495994e60f795ad591554c92
Ben Cavins <bencavins@gmail.com>
2018-05-27 15:22:17 -0400

    add timer; add Drill classes

--- music/scale_degree_drill.py
+++ music/scale_degree_drill.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 class Scale(object):
@@ -34,31 +35,82 @@ MAJOR_SCALES = [
 ]
 
 
-def random_scale():
-    return random.choice(MAJOR_SCALES)
+class Drill(object):
+    def __init__(self, **kwargs):
+        self.total = 0
+        self.correct = 0
+        self.time_limit = kwargs.get('time_limit', 0)  # in seconds
+        self.start_time = None
+
+    def setup(self):
+        """Called at the beginning of a drill run"""
+
+        self.start_time = time.time()
+
+    def run(self):
+        self.setup()
+
+        while self.can_run():
+            try:
+                correct = self.drill()
+            except EOFError:  # thrown on ctrl+d
+                break
+
+            if correct:
+                self.correct += 1
+            self.total += 1
+
+        self.tearDown()
+
+    def drill(self):
+        """Return True if correct, False if incorrect"""
+        return False
+
+    def tearDown(self):
+        """Called at the end of a drill run"""
+        print('''
+Results:
+{0}/{1} correct: {2}%
+'''.format(self.correct, self.total, (float(self.correct) / self.total) * 100))
+
+    def can_run(self):
+        """Drill runs as long as this is true"""
+
+        if self.time_limit:
+            return self.is_out_of_time()
+        else:
+            return True
+
+    def is_out_of_time(self):
+        elapsed = time.time() - self.start_time
+        return elapsed < self.time_limit
 
 
-def random_degree():
-    return random.randint(1, 7)
+class ScaleDegreeDrill(Drill):
 
-
-def main():
-    total = 0
-    correct = 0
-
-    while True:
-        scale = random_scale()
-        degree = random_degree()
+    def drill(self):
+        scale = self.random_scale()
+        degree = self.random_degree()
 
         prompt = 'What is scale degree {0} for {1}? '.format(degree, scale.tonic)
         answer = input(prompt)
 
-        if answer == scale.degree(degree):
-            correct += 1
-        total += 1
         print('Answer: {0}'.format(scale.degree(degree)))
 
-        print('{0}/{1}'.format(correct, total))
+        return answer == scale.degree(degree)
+
+    @staticmethod
+    def random_scale():
+        return random.choice(MAJOR_SCALES)
+
+    @staticmethod
+    def random_degree():
+        return random.randint(1, 7)
+
+
+def main():
+    drill = ScaleDegreeDrill(time_limit=60)
+    drill.run()
 
 
 if __name__ == '__main__':
